0143f46397e51f836b88c75262916b7b80b86eef
foreveralone1608 <hotranphihung@gmail.com>
2020-04-12 17:04:13 +0700

    3333

--- queue.py
+++ queue.py
@@ -1,16 +1,16 @@
-class stack:
+class queue:
     def __init__(self):
         self.items = []
     def push(self, item):
         return self.items.append(item)
     def pop(self):
-        return self.items.pop()
+        return self.items.pop(0)
     def delay(self):
         return self.items
-#test
-a = stack()
-a.push(4)
+
+a = queue()
 a.push(5)
-a.push(6)
+a.push(7)
+a.push(3)
 a.pop()
 print(a.delay())
